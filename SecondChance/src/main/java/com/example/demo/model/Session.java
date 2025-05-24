ae092bd73816366fde1a9978199300e3b2e0b9d1
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 17:18:16 +0530

    relationship session and presenter;\

--- SecondChance/src/main/java/com/example/demo/model/Session.java
+++ SecondChance/src/main/java/com/example/demo/model/Session.java
@@ -2,9 +2,13 @@ package com.example.demo.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Session {
@@ -14,4 +18,15 @@ public class Session {
 	private String description;
 	private LocalTime time;
 	private LocalDate date; 
+	
+	
+	@ManyToOne
+	@JoinColumn(name="workshop_id")
+	private Workshops workshops;
+	
+	@ManyToMany(mappedBy = "sessions")
+	private List<Presenter> presenters;
+	
+	@ManyToMany(mappedBy = "sessions")
+	private List<UnderGraduate> undergraduates;
 }
