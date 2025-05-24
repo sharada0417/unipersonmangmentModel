03aa4abf6e4f54d023ca2b128cc2c4f1a79e8742
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 17:09:14 +0530

    create workshop model;

--- SecondChance/src/main/java/com/example/demo/model/Workshops.java
+++ SecondChance/src/main/java/com/example/demo/model/Workshops.java
@@ -1,5 +1,16 @@
 package com.example.demo.model;
 
-public class Workshops {
+import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Workshops {
+	@Id
+	private int id;
+	private String description;
+	private String name;
+	private LocalDate startDate;
+	private LocalDate endDate;
 }
