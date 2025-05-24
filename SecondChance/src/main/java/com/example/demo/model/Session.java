3a96a354e38aaca1f07a2d153aa762794c16c83a
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 17:03:13 +0530

    create session model;

--- SecondChance/src/main/java/com/example/demo/model/Session.java
+++ SecondChance/src/main/java/com/example/demo/model/Session.java
@@ -1,5 +1,17 @@
 package com.example.demo.model;
 
-public class Session {
+import java.time.LocalDate;
+import java.time.LocalTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Session {
+	@Id
+	private String id;
+	private String name;
+	private String description;
+	private LocalTime time;
+	private LocalDate date; 
 }
