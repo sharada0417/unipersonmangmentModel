5cf67e5afdc493bd23ac829a2acc30badfb45e5e
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 17:01:00 +0530

    create presenter model

--- SecondChance/src/main/java/com/example/demo/model/Presenter.java
+++ SecondChance/src/main/java/com/example/demo/model/Presenter.java
@@ -1,5 +1,19 @@
 package com.example.demo.model;
 
-public class Presenter {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
+public class Presenter {
+	@Id
+	private String id;
+	private String name;
+	private String email;
+	private String gender;
+	private String phone;
+	private String affilication;
+	private String country;
+	private String jobTitle;
+	
+	
 }
