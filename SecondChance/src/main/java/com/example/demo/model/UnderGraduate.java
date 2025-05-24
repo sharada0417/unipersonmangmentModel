dff851a949985447be585d24939357e0c4fcbb30
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 17:25:40 +0530

    create undergraduate relationships

--- SecondChance/src/main/java/com/example/demo/model/UnderGraduate.java
+++ SecondChance/src/main/java/com/example/demo/model/UnderGraduate.java
@@ -1,7 +1,12 @@
 package com.example.demo.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 @Entity
 public class UnderGraduate {
@@ -13,4 +18,12 @@ public class UnderGraduate {
 	private String phone;
 	private String degree;
 	private String university;
+	
+	@ManyToMany
+	@JoinTable(
+		name = "under_graduate_sessions",
+		joinColumns = @JoinColumn(name="under_graduate_id"),
+		inverseJoinColumns = @JoinColumn(name="session_id")
+	)
+	private List<Session> sessions;
 }
