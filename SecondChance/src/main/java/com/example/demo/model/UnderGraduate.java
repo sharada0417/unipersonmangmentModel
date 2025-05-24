ea00fca059db28eefc23a36bc80cc14483e502d4
sharada417 <sharada.kanchana2001@gmail.com>
2025-05-24 17:06:57 +0530

    create undergraduate Model

--- SecondChance/src/main/java/com/example/demo/model/UnderGraduate.java
+++ SecondChance/src/main/java/com/example/demo/model/UnderGraduate.java
@@ -1,5 +1,16 @@
 package com.example.demo.model;
 
-public class UnderGraduate {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
+public class UnderGraduate {
+	@Id
+	private String id;
+	private String name;
+	private String email;
+	private String gender;
+	private String phone;
+	private String degree;
+	private String university;
 }
